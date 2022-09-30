78fd28353768ed6ef839c9bfad98a99c6182723d
Tushar Goyal <tushar462024@gmail.com>
2022-10-01 03:51:41 +0530

    Make changes to the configs as per latest nvchad changes

--- .config/nvim/nvchad/custom/chadrc.lua
+++ .config/nvim/nvchad/custom/chadrc.lua
@@ -1,22 +1,14 @@
 local M = {}
-local override = require "custom.override"
 
-M.plugins = {
-  override = {
-    ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
-    ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
-    ["lukas-reineke/indent-blankline.nvim"] = override.blankline,
-    ["goolord/alpha-nvim"] = override.alpha,
-    ["williamboman/mason.nvim"] = override.mason,
-  },
-
-  user = require "custom.plugins",
-}
+M.plugins = require "custom.plugins"
 
 M.ui = {
+  -- theme stuff
   theme = "nightowl",
-  hl_add = require("custom.highlights").new_hlgroups,
   theme_toggle = { "nightowl", "gruvbox_light" },
+
+  -- highlight groups!
+  hl_add = require("custom.highlights").new_hlgroups,
   hl_override = require("custom.highlights").overriden_hlgroups,
 }
 
